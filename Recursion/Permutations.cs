6b555993ee3aaa60982eb92d8c365eb05d627feb
seven.duan <seven.j.duan@gmail.com>
2023-11-08 23:46:51 +1100

    study DFS

--- Recursion/Permutations.cs
+++ Recursion/Permutations.cs
@@ -1,4 +1,3 @@
-//https://leetcode.com/problems/permutations/
 public class SolutionPermute {
     public IList<IList<int>> Permute(int[] nums) {
         if (nums == null || nums.Length == 0)
@@ -6,25 +5,24 @@ public class SolutionPermute {
         
         IList<IList<int>> result = new List<IList<int>>();
         IList<int> list = new List<int>();
-        bool[] used = new bool[nums.Length];
 
-        SubsetsHelper(result, list, used, nums);
+        SubsetsHelper(result, list, nums);
         return result;
     }
 
-    private void SubsetsHelper(IList<IList<int>> result, IList<int> list, bool[] used, int[] nums) {
-        if (list.Count == nums.Length)
+    private void SubsetsHelper(IList<IList<int>> result, IList<int> list, int[] nums) {
+        if (list.Count == nums.Length) {
             result.Add(new List<int>(list));
+            return;
+        }
         
         for (int i = 0; i < nums.Length; i++) {
-            if (!used[i])
-            {
-                used[i] = true;
-                list.Add(nums[i]);
-                SubsetsHelper(result, list, used, nums);
-                list.RemoveAt(list.Count - 1);
-                used[i] = false;
-            }
+            if (list.Contains(nums[i]))
+                continue;
+
+            list.Add(nums[i]);
+            SubsetsHelper(result, list, nums);
+            list.RemoveAt(list.Count - 1);
         }
     }
 }
