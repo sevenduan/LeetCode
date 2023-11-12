56ae6415916fe4483903ac3344e19c1ae3c5c5dc
seven.duan <seven.j.duan@gmail.com>
2023-11-12 11:34:06 +1100

    PermuteUnique

--- Recursion/PermuteUnique.cs
+++ Recursion/PermuteUnique.cs
@@ -1,12 +1,12 @@
 //https://leetcode.com/problems/permutations-ii/
 public class SolutionPermuteUnique {
-    public IList<IList<int>> PermuteUnique(int[] nums) {        
+    public IList<IList<int>> PermuteUnique(int[] nums) {      
+        IList<IList<int>> result = new List<IList<int>>();  
         if (nums == null || nums.Length == 0)
-            return null;
+            return result;
 
         //sort
         Array.Sort(nums);
-        IList<IList<int>> result = new List<IList<int>>();
         IList<int> list = new List<int>();
         bool[] used = new bool[nums.Length];
 
@@ -22,17 +22,17 @@ public class SolutionPermuteUnique {
         
         for (int i = 0; i < nums.Length; i++)
         {
+            if (used[i])
+                continue;
+
             if (i > 0 && nums[i] == nums[i-1] && !used[i-1])
                 continue;
 
-            if (!used[i])
-            {
-                used[i] = true;
-                list.Add(nums[i]);
-                SubsetsHelper(result, list, used, nums);
-                list.RemoveAt(list.Count - 1);
-                used[i] = false;
-            }
+            used[i] = true;
+            list.Add(nums[i]);
+            SubsetsHelper(result, list, used, nums);
+            list.RemoveAt(list.Count - 1);
+            used[i] = false;
         }
     }
 }
