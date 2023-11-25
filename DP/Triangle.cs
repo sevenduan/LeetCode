1d946991db49c4141fad36f64eb6cd44a0ba6cf8
seven.duan <seven.j.duan@gmail.com>
2023-11-25 22:21:13 +1100

    rolling array

--- DP/Triangle.cs
+++ DP/Triangle.cs
@@ -24,3 +24,31 @@ public class SolutionMinimumTotal {
         return answer;
     }
 }
+
+// Rolling Array
+public class SolutionRolling {
+    public int MinimumTotal(IList<IList<int>> triangle) {
+        int n = triangle.Count;
+
+        int[,] f = new int[2,n];
+
+        f[0, 0] = triangle[0][0];
+
+        for (int i = 1; i < n; i++) {
+            int m = i % 2;
+            f[m, 0] = f[1 - m, 0] + triangle[i][0];
+            f[m, i] = f[1 - m, i - 1] + triangle[i][i];
+            for (int j = 1; j < i; j++) {
+                f[m, j] = Math.Min(f[1 - m, j], f[1 - m, j - 1]) + triangle[i][j];
+            }
+        }
+
+        int last = (n - 1) % 2;
+        int answer = f[last, 0];
+        for (int i = 1; i < n; i++) {
+            answer = Math.Min(answer, f[last, i]);
+        }
+
+        return answer;
+    }
+}
