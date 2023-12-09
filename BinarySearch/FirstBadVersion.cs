6ad59d71c9ffb77f95e11fe925d409da7164e2aa
seven.duan <seven.j.duan@gmail.com>
2023-12-09 20:42:40 +1100

    daily checkin

--- BinarySearch/FirstBadVersion.cs
+++ BinarySearch/FirstBadVersion.cs
@@ -1,22 +1,22 @@
-/* The isBadVersion API is defined in the parent class VersionControl.
-      bool IsBadVersion(int version); */
+// /* The isBadVersion API is defined in the parent class VersionControl.
+//       bool IsBadVersion(int version); */
 
-public class Solution : VersionControl {
-    public int FirstBadVersion(int n) {        
-        int start = 1;
-        int end = n;
+// public class Solution : VersionControl {
+//     public int FirstBadVersion(int n) {        
+//         int start = 1;
+//         int end = n;
 
-        while (start + 1 < end) {
-            int mid = start + (end - start) / 2;
-            if (IsBadVersion(mid))
-                end = mid;
-            else
-                start = mid;
-        }
+//         while (start + 1 < end) {
+//             int mid = start + (end - start) / 2;
+//             if (IsBadVersion(mid))
+//                 end = mid;
+//             else
+//                 start = mid;
+//         }
 
-        if (IsBadVersion(start))
-            return start;
-        else
-            return end;
-    }
-}
+//         if (IsBadVersion(start))
+//             return start;
+//         else
+//             return end;
+//     }
+// }
